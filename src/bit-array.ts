555a5cb3b60eae6c8a4d1f869575f1eb9a6d4814
Déric Dallaire <dallaire.deric@gmail.com>
2018-03-27 22:31:02 -0400

    Added comments and exceptions

--- src/bit-array.ts
+++ src/bit-array.ts
@@ -16,7 +16,7 @@ export default class BitArray {
 
   push(value: bit) {
     if (this._pointer == this._size) {
-      throw "Array max size reached";
+      throw `Bit array max size reached (${this._size})`;
     }
     // create a mask for current index.
     const mask = this._createMask(this._pointer);
@@ -36,30 +36,35 @@ export default class BitArray {
     this._pointer++;
   }
 
+  // Returns the bit at a given index
   atIndex(index: number): bit {
     if (index > this._size) {
-      return null;
+      throw `Index (${index}) exceeds the size of the bit array (${this._size})`;
     }
 
     const mask = this._createMask(index);
     return this._matchesMask(mask, this._array[this._arrayIndex(index)]) ? 1 : 0;
   }
   
+  // Checks whether a value has a bit of 1 at the mask position
   private _matchesMask(mask: byte, value: byte) {
     // returns true if bit is set
     return (mask & value) != 0;
   }
 
+  // Creates a mask for a given index
   private _createMask(index: number): byte {
     // supposing the index is 2, the created mask is
     // 00100000
     return  1 << (this._bitPerIndex - 1) - this._arrayIndexOffset(index);
   }
 
+  // Gets the Uint8Array index
   private _arrayIndex(index: number) {
     return Math.floor(index / this._bitPerIndex);
   }
 
+  // Gets the offset within the Uint8Array
   private _arrayIndexOffset(index: number) {
     return index % this._bitPerIndex;
   }
