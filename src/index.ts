a9ff2967f7662a288a58538193c4379eb09bc613
Déric Dallaire <dallaire.deric@gmail.com>
2018-03-29 19:51:43 -0400

    Added entire alphabet with an height of 8

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import { Alphabet } from './alphabet';
 const board = new Board(2);
 const dictionary = new CharacterDictionary(Alphabet);
 
-board.load("aaaaaaaa", dictionary);
+board.load("abcdefghijklmnopqrstuvwxyz", dictionary);
 
 const matrix = new ScrollingMatrix(board);
 matrix.stepParameters({  
@@ -26,36 +26,9 @@ matrix.stepParameters({
 });
 
 matrix.play();
-/*
-let arr = new BitArray(12);
-arr.push(1);
-arr.push(1);
-arr.push(1);
-arr.push(1);
-arr.push(1);
-arr.push(1);
-arr.push(1);
-arr.push(1);
-arr.push(1);
-arr.push(1);
-arr.push(1);
-arr.push(1);
-console.log(arr.atIndex(0));
-console.log(arr.atIndex(1));
-console.log(arr.atIndex(2));
-console.log(arr.atIndex(3));
-console.log(arr.atIndex(4));
-console.log(arr.atIndex(5));
-console.log(arr.atIndex(6));
-console.log(arr.atIndex(7));
-console.log(arr.atIndex(8));
-console.log(arr.atIndex(9));
-console.log(arr.atIndex(10));
-console.log(arr.atIndex(11));
-*/
-/*
+
 setTimeout(function(){ matrix.pause() }, 3000);
 setTimeout(function(){ matrix.resume() }, 4000);
-setTimeout(function(){ matrix.stop() }, 5000);
-setTimeout(function(){ matrix.play() }, 6000);
-*/
+setTimeout(function(){ board.load("a", dictionary); }, 4500);
+setTimeout(function(){ matrix.stop() }, 6000);
+setTimeout(function(){ matrix.play() }, 7000);
