1322a59ad94014fee7fb4cf222660bdf9c879839
Déric Dallaire <dallaire.deric@gmail.com>
2018-04-01 12:43:53 -0400

    Removed unused function from basic demo

--- src/demos/basics/index.ts
+++ src/demos/basics/index.ts
@@ -30,24 +30,6 @@ const myCustomAppearance = (display: any) => {
     document.getElementById("root").innerHTML = output; 
 };
 
-
-const canvaAppearance = (display: any) => {
-  var c = document.getElementById("myCanvas") as HTMLCanvasElement;
-  var ctx = c.getContext("2d");
-  for(var i = 0; i < display.length; i++) {
-      for(var j = 0; j < display[i].length; j++) {
-
-        ctx.beginPath();
-        ctx.fillStyle= display[i][j] == 1 ? "#00B16A" : "#22313F";
-        ctx.rect(j*40, i*40, 40, 40);
-        ctx.fill();
-        ctx.lineWidth = 1;
-        ctx.strokeStyle = "#67809F";
-        ctx.stroke();
-      }
-  }
-};
-
 panel.events({
   onPanelUpdate: (display) => { 
     myCustomAppearance(display);
