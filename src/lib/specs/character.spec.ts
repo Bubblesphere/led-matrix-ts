6c13ebb3bb87cb6e89f864830b304de9b92c7956
Déric Dallaire <dallaire.deric@gmail.com>
2018-09-23 00:02:14 -0400

    Added character specs and some specs for the panel

---
+++ src/lib/specs/character.spec.ts
@@ -0,0 +1,101 @@
+import Character from "../character";
+import BitArray from "../bit-array";
+
+describe('testing Character constructor', () => {
+    test('Should throw an exception if the output is smaller than the character\'s width', () => {
+        expect(() => {
+            return new Character(['a'], new BitArray([0]), 2);
+        }).toThrow();
+    });
+
+    test('Should throw an exception if the output isn\'t divisible by the character\'s width', () => {
+        expect(() => {
+            return new Character(['a'], new BitArray([0, 0, 0]), 2);
+        }).toThrow();
+    });
+});
+
+describe('testing getColumn', () => {
+    let character: Character;
+
+    beforeEach(() => {
+        character = new Character(['a'], new BitArray([1, 0, 0, 1]), 2);
+    });
+
+    test('Should throw an exception if the index is negative', () => {
+        expect(() => {
+            character.getColumn(-1);
+        }).toThrow();
+    });
+
+    test('Should throw an exception if the index is greater than the width', () => {
+        expect(() => {
+           character.getColumn(2);
+        }).toThrow();
+    });
+
+    test('Should return valid columns for a character', () => {
+        expect(character.getColumn(0)).toEqual([1, 0]);
+        expect(character.getColumn(1)).toEqual([0, 1]);
+    });
+});
+
+describe('testing getRow', () => {
+    let character: Character;
+
+    beforeEach(() => {
+        character = new Character(['a'], new BitArray([1, 0, 0, 1]), 2);
+    });
+
+    test('Should throw an exception if the index is negative', () => {
+        expect(() => {
+            character.getRow(-1);
+        }).toThrow();
+    });
+
+    test('Should throw an exception if the index is greater than the height', () => {
+        expect(() => {
+           character.getRow(2);
+        }).toThrow();
+    });
+
+    test('Should return valid rows for a character', () => {
+        expect(character.getRow(0)).toEqual([1, 0]);
+        expect(character.getRow(1)).toEqual([0, 1]);
+    });
+});
+
+describe('testing properties', () => {
+    let character: Character;
+
+    beforeEach(() => {
+        character = new Character(['a'], new BitArray([1, 0, 0, 1]), 2);
+    });
+
+    test('Should return the correct width', () => {
+        expect(character.width).toBe(2);
+    });
+
+    test('Should return the correct height', () => {
+        expect(character.height).toBe(2);
+    });
+
+    test('Should return the correct patterns', () => {
+        expect(character.patterns).toEqual(['a']);
+    });
+
+    test('Should return the correct output', () => {
+        expect(character.output).toEqual(new BitArray([1, 0, 0, 1]));
+    });
+});
+
+describe('testing hasPattern', () => {
+    test('Should return false if the character doesn\'t have the pattern', () => {
+        const character = new Character(['a'], new BitArray([1, 0, 0, 1]), 2);
+        expect(character.hasPattern('b')).toBeFalsy();
+    });
+    test('Should return true if the character has the pattern', () => {
+        const character = new Character(['a'], new BitArray([1, 0, 0, 1]), 2);
+        expect(character.hasPattern('a')).toBeTruthy();
+    });
+});
---
+++ src/lib/specs/panel.spec.ts
@@ -0,0 +1,181 @@
+import Panel from "../panel";
+import Board from "../board";
+import SideScrollingPanel from "../panels/side-scrolling-panel";
+import ASCIIRenderer from "../rendering/ascii-renderer";
+import Renderer from "../rendering/renderer";
+import CharacterDictionary from "../character-dictionary";
+import Character from "../character";
+import BitArray from "../bit-array";
+
+
+let panel: Panel;
+let renderer: Renderer;
+let board: Board;
+
+beforeEach(() => {
+    board = new Board({
+        padding: [0],
+        spacing: 0,
+    });
+
+    const dict = new CharacterDictionary();
+    dict.add([
+        new Character(['a'], new BitArray([0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1]), 14)
+    ])
+
+    board.load('aaaaaaaaaaaa', dict);
+
+    renderer = new ASCIIRenderer({
+        characterBitOn: 'X',
+        characterBitOff: ' ',
+        element: document.getElementById("led-matrix")
+    });
+
+    panel = new SideScrollingPanel({
+        board: board,
+        renderer: renderer,
+        increment: 1,
+        fps: 60,
+        width: 80,
+        reverse: false
+    });
+});
+
+describe('testing width', () => {
+    test('Should throw an error when setting a null width', () => {
+        expect(() => { 
+            panel.width = null;
+        }).toThrow();
+    });
+
+    test('Should throw an error when setting a negative width', () => {
+        expect(() => { 
+            panel.width = -1;
+        }).toThrow();
+    });
+
+    test('Should set width when it\'s a positive number', () => {
+        panel.width = 1;
+        expect(panel.width).toBe(1);
+    });
+});
+
+describe('testing fps', () => {
+    test('Should throw an error when setting a null fps', () => {
+        expect(() => { 
+            panel.fps = null;
+        }).toThrow();
+    });
+
+    test('Should throw an error when setting a negative fps', () => {
+        expect(() => { 
+            panel.fps = -1;
+        }).toThrow();
+    });
+
+    test('Should throw an error when setting an fps greater than 60', () => {
+        expect(() => { 
+            panel.fps = 61;
+        }).toThrow();
+    });
+
+    test('Should set fps when it\'s a positive number', () => {
+        panel.fps = 1;
+        expect(panel.fps).toBe(1);
+    });
+});
+
+describe('testing board', () => {
+    test('Should throw an error when setting a null board', () => {
+        expect(() => { 
+            panel.board = null;
+        }).toThrow();
+    });
+
+    test('Should set board when it\'s a Board', () => {
+        panel.board = board;
+        expect(panel.board).toEqual(board);
+    });
+});
+
+describe('testing increment', () => {
+    test('Should throw an error when setting a null increment', () => {
+        expect(() => { 
+            panel.increment = null;
+        }).toThrow();
+    });
+
+    test('Should throw an error when setting a negative increment', () => {
+        expect(() => { 
+            panel.increment = -1;
+        }).toThrow();
+    });
+
+    test('Should set increment when it\'s a positive number', () => {
+        panel.increment = 1;
+        expect(panel.increment).toBe(1);
+    });
+});
+
+describe('testing renderer', () => {
+    test('Should throw an error when setting a null renderer', () => {
+        expect(() => { 
+            panel.renderer = null;
+        }).toThrow();
+    });
+
+    test('Should set renderer when it\'s a Renderer', () => {
+        panel.renderer = renderer;
+        expect(panel.renderer).toEqual(renderer);
+    });
+});
+
+describe('testing reverse', () => {
+    test('Should throw an error when setting a null reverse', () => {
+        expect(() => { 
+            panel.reverse = null;
+        }).toThrow();
+    });
+
+    test('Should set reverse when it\'s a boolean value', () => {
+        panel.reverse = true;
+        expect(panel.reverse).toBeTruthy();
+    });
+});
+
+
+describe('ui', () => {
+    document.body.innerHTML = '<div id="led-matrix" style="font-family: monospace; white-space: pre;"></div>'; 
+
+    describe('testing play', () => {
+        test('Should change the index at least once after 200ms', (done) => {
+            panel.play();
+            setTimeout(() => {
+                expect(panel.index).not.toBe(0);
+                done();
+            }, 200);
+        });
+    });
+    
+    describe('testing stop', () => {
+        test('Should reset the index back to 0 when stopping', (done) => {
+            panel.play();
+            setTimeout(() => {
+                expect(panel.index).not.toBe(1);
+                panel.stop();
+                expect(panel.index).toBe(0);
+                done();
+            }, 200);
+        });
+    });
+});
+
+
+/*
+resume
+pause
+seek
+PanelUpdateBit
+PanelUpdate
+ReachingBoundary
+*/
