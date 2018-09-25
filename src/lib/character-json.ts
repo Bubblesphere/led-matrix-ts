f259bf0681e128ade9e0385422468b373ef22548
Déric Dallaire <dallaire.deric@gmail.com>
2018-09-25 14:19:55 -0400

    Added  size to LedMatrix

--- src/lib/character-json.ts
+++ src/lib/character-json.ts
@@ -1,16 +1,17 @@
 import { CharactersJSONSchema } from "./types";
 import Character from "./character";
 import BitArray, { bit } from "./bit-array";
+import CharacterSizer from "./character-sizer";
 
 export default class CharactersJSON {
-    static import(path: string, success: (content: Character[]) => any) {
+    static import(path: string, size: number, success: (content: Character[]) => any) {
         const file = new XMLHttpRequest();
         file.open("GET", path, false);
         file.onreadystatechange = () =>
         {
             if(file.readyState === 4) {
                 if(file.status === 200 || file.status == 0) {
-                    success(CharactersJSON.parse(file.responseText));
+                    success(CharactersJSON.parse(file.responseText, size));
                 }
             }
             // TODO: Error management
@@ -22,10 +23,17 @@ export default class CharactersJSON {
         // TODO: Implement
     }
 
-    static parse(json: string): Character[] {
+    static parse(json: string, size: number): Character[] {
         // TODO: Validation
         const data = JSON.parse(json) as CharactersJSONSchema;
-        return data.characters.map(x => new Character(x.patterns, new BitArray(x.output.map(x => x as bit)), x.width))
+
+        return data.characters.map(x => {
+            const characterRaw = x.output.map(x => x as bit);
+            const w = x.width;
+            const h = x.output.length / x.width;
+            const character = CharacterSizer.resize(characterRaw, w, h, w * size, h * size);
+            return new Character(x.patterns, new BitArray(character), w * size) 
+        });
     }
 
     static stringify(characters: Character[]): string {
