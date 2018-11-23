7feb3fcf1f6346ca5fa36fbddb4366645a61f2e8
Déric Dallaire <dallaire.deric@gmail.com>
2018-11-23 16:06:32 -0500

    Added missing imports

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,27 @@
 export * from './lib/types';
 
 // core
-export * from './lib/core/board';
-export * from './lib/core/character';
-export * from './lib/core/character-dictionary';
-export * from './lib/core/character-json';
-export * from './lib/core/character-sizer';
 export * from './lib/core/led-matrix';
-export * from './lib/core/panel';
+
 export * from './lib/core/scrollers/scroller';
 export * from './lib/core/scrollers/side-scroller';
 export * from './lib/core/scrollers/vertical-scroller';
+export * from './lib/core/board';
+export * from './lib/core/character-dictionary';
+export * from './lib/core/character-json';
+export * from './lib/core/character-sizer';
+export * from './lib/core/character';
+export * from './lib/core/panel';
 
 // player
-export * from './lib/player/panel-player';
-export * from './lib/player/rendering/renderer-builder';
+export * from './lib/player/led-matrix-player';
+
 export * from './lib/player/rendering/ascii-renderer';
 export * from './lib/player/rendering/canva-renderer';
 export * from './lib/player/rendering/canva-renderers';
+export * from './lib/player/rendering/renderer-builder';
 export * from './lib/player/rendering/renderer';
+export * from './lib/player/panel-player';
 
 //utils
-export * from './lib/utils/event'; 
+export * from './lib/utils/event';
